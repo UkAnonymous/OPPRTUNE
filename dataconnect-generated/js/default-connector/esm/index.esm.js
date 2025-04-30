3bdab3f080db6c5b2b644f178bf92d790a335af5
UkAnonymous <sahilsutar052@gmail.com>
2025-04-30 15:27:56 +0530

    initial

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'projectcollege',
+  location: 'us-central1'
+};
---
+++ node_modules/@firebasegen/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'projectcollege',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
